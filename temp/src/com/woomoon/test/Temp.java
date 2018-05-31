dc5694c31d9ef178218966bdec4075dfe390cfeb
woomoon <woomoonjava@qq.com>
2018-05-31 16:55:11 +0800

    user entity commit

---
+++ temp/src/com/woomoon/test/Temp.java
@@ -0,0 +1,20 @@
+package com.woomoom.test;
+
+public class Temp {
+
+    public static void main(String[] args) {
+
+        System.out.println("test git commit ");
+
+        int a = 3;
+        add(a);
+        System.out.println("a = "+a);
+    }
+
+
+    public static void add(int a) {
+        a += a;
+    }
+
+
+}
